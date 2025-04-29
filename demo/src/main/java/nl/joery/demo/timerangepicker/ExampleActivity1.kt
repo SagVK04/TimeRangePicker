00a972a8d8ae36884641f0e4ddf3c4c6c522d413
Sagnik <metiyasagnik692@gmail.com>
2025-04-29 21:47:12 +0530

    Added example with new time-range-picker for testing

---
+++ demo/src/main/java/nl/joery/demo/timerangepicker/ExampleActivity1.kt
@@ -0,0 +1,50 @@
+package com.example.myapplication
+
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.github.ybq.android.spinkit.sprite.Sprite
+import com.github.ybq.android.spinkit.style.DoubleBounce
+import com.github.ybq.android.spinkit.style.Wave
+import nl.joery.timerangepicker.TimeRangePicker
+
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val progressBar = findViewById<View>(R.id.spin_kit) as ProgressBar
+        val wave: Sprite = Wave()
+        progressBar.indeterminateDrawable = wave
+        binding.picker.setOnTimeChangeListener(object : TimeRangePicker.OnTimeChangeListener {
+            override fun onStartTimeChange(startTime: TimeRangePicker.Time) {
+                Log.d("TimeRangePicker", "Start time: " + startTime)
+            }
+
+            override fun onEndTimeChange(endTime: TimeRangePicker.Time) {
+                Log.d("TimeRangePicker", "End time: " + endTime.hour)
+            }
+
+            override fun onDurationChange(duration: TimeRangePicker.TimeDuration) {
+                Log.d("TimeRangePicker", "Duration: " + duration.hour)
+            }
+        })
+    }
+}
